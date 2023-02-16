515c610216881a606075b3b50da22c6c3fb381ea
Zeki Ahmed <iltcodep@gamil.com>
2023-02-16 05:56:02 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A programe that uses printf() function
+ *
+ * Return: 0 (Succese)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
