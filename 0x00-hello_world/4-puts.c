eb29ba0b25f6fa416d6e34d4a2bfe1c28d09f145
Zeki Ahmed <iltcodep@gamil.com>
2023-02-16 04:54:18 -0800

    Hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A programe that uses put() function
+ *
+ * Return: 0 (Succese)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+};
