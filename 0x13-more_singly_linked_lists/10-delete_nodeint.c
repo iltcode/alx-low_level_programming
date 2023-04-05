8404e292427223ea53919d06280d8bbf47013b0a
iltcode <iltcodep@gmail.com>
2023-04-05 14:46:29 -0700

    updated

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,32 +1,47 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - returns the node at a certain index in a linked list
- * @head: first node in the linked list
- * @index: index of the node to return
- *
- * Return: pointer to the node we're looking for, or NULL
- */
+* get_nodeint_at_index - returns the node at a certain index in a linked list
+* @head: first node in the linked list
+* @index: index of the node to return
+*
+* Return: pointer to the node we're looking for, or NULL
+*/
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *newh;
-        listint_t *p = *head;
-        unsigned int len = 0;
+listint_t *del = *head;
+listint_t *p = *head;
+unsigned int len = 1;
 
-        while (p != NULL)
-        {
-                if (len == index)
-                {
-			newh = (*head)->next;
-			free(*head);
-			*head = newh;
-                        return (1);
-                }
+if (!*head)
+{
+return (-1);
+}
 
-                len++;
+if (index == 0)
+{
+*head = del->next;
+free(del);
+del = NULL;
+return (1);
+}
 
-                p = p->next;
-        }
+while ((del->next) != NULL)
+{
+if (len == index)
+{
+	p->next = del->next;
+	free(del);
+	del = NULL;
+	return (1);
+}
 
-        return (-1);
+
+p = del;
+del = del->next;
+len++;
+
+}
+
+return (-1);
 }
