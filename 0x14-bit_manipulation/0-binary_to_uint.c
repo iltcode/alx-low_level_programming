b2c6ff74458fb274b756a2a031df64fa630b51ec
iltcode <iltcodep@gmail.com>
2023-04-06 19:24:19 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,10 +17,7 @@ unsigned int binary_to_uint(const char *b)
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
 
-		if (b[len] == '1')
-		{
-		sum += mul;
-		}
+		sum += (b[len] - '0') *mul;
 		mul *= 2;
 	}
 
