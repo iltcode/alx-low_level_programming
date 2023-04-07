bbc94b480fdd491a3137a0036c7d0c52a27cad5b
iltcode <iltcodep@gmail.com>
2023-04-06 19:22:39 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,45 +1,28 @@
 #include "main.h"
 
 /**
-* binary_to_uint - convert a binary number to an unsigned int
-* @b: char string
-* Return: converted decimal number or 0 if there is an unconvertable char
-*/
+ * binary_to_uint - convert a binary number to an unsigned int
+ * @b: char string
+ * Return: converted decimal number or 0 if there is an unconvertable char
+ */
 
 unsigned int binary_to_uint(const char *b)
 {
-int x;
-int len = strlen(b);
-unsigned int sum = 0;
-unsigned int val = 1;
-
-if (b == NULL)
-{
-
-return (0);
-}
-
-for (x = (len - 1); x >= 0 ; x--)
-{
-if (b[x] == '1')
-{
-sum = sum + val;
-
-}
-val = val * 2;
-}
+	unsigned int sum = 0, mul = 1;
+	int len = strlen(b);
 
 
-for (x = (len - 1); x >= 0 ; x--)
-{
-if (b[x] != '0' && b[x] != '1')
-{
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
 
-return (0);
+		if (b[len] == '1')
+		{
+		sum += mul;
+		}
+		mul *= 2;
+	}
 
-}
-}
-
-
-return (sum);
+	return (sum);
 }
