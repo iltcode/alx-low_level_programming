07aa8bd63e12c1a3eaa2585a084a8b27fd8ab0ec
iltcode <iltcodep@gmail.com>
2023-04-11 15:09:52 -0700

    updated !filename.... (-1)

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 
 	_o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
